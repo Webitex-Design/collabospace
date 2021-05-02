a970263a00596c89d13567e807bb43bc39b912d8
Sid Dinkar <sid@Sids-Macbook.local>
2021-05-01 22:54:13 -0700

    login routes

---
+++ collabospace-api/routes/posts.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+require("dotenv/config");
+
+
+
+router.post("/new-post")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
