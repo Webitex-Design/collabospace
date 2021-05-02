c6e608be4f1ed0aac7c3d10b207234e07e93a328
Soham Vohra <soham.vohra@gmail.com>
2021-05-02 13:42:52 -0700

    done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  entryType = 'Login';
+
+  onLogin(){
+    this.entryType = 'Login'
+  }
+  onSignup(){
+    this.entryType = 'Sign up'
+  }
 
   constructor() { }
 
