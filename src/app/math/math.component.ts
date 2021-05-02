86a51f86d36d8ffd5e04065a34b8d125373fb987
Soham Vohra <soham.vohra@gmail.com>
2021-05-02 00:48:06 -0700

    done

---
+++ src/app/math/math.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-math',
+  templateUrl: './math.component.html',
+  styleUrls: ['./math.component.css']
+})
+export class MathComponent implements OnInit {
+  topic:string = '';
+  title:string = '';
+  body: string = '';
+
+  entries = [];
+
+  constructor() { }
+
+  onSubmit(){
+    this.entries.push({topic: this.topic, title: this.title, body: this.body});
+    this.topic = '';
+    this.title = '';
+    this.body = '';
+    console.log(this.entries)
+  };
+
+  ngOnInit(): void {
+  }
+
+}
